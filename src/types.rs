8af07096189b225194783df5eec9864bac01a7c0
Éric Spérano <eric.sperano@gmail.com>
2025-11-19 21:05:30 -0800

    wip

--- src/types.rs
+++ src/types.rs
@@ -3,12 +3,13 @@
 /// This module contains type definitions that are shared between
 /// the library (commands, tui) and the binary (main.rs).
 
-/// Global constants
-pub const NHL_LEAGUE_ABBREV: &str = "NHL";
-
 enum TeamNameFormat {
     Abbreviated,
     City,
     Common,
     Full,
 }
+
+// impl TeamNameFormat {
+//     fn display(&self, )
+// }
